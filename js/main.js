1628b33cb17013106566f0d4d525d38cbee3b415
jaime7981 <jmfernandez1@miaundes.cl>
2023-04-11 16:52:32 -0400

    navbar append buttons by js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,59 @@
 
-window.addEventListener("load", function (e) {
-    console.log("loaded DOM")
-    let gamesButton = document.getElementById("games-button");
-    let logoutButton = document.getElementById("logout-button");
-    let loginButton = document.getElementById("login-button");
-    let registerButton = document.getElementById("register-button");
+function loadErrorBlock() {
+    let sectionCenter = document.getElementsByClassName("section-center");
+    let errorBlock = document.createElement("div");
+    errorBlock.className = "show-errors";
+    errorBlock.id = "show-form-errors";
 
+    if (sectionCenter.length >= 1) {
+        sectionCenter[0].appendChild(errorBlock);
+    }
+}
+
+function createNavButton(name) {
+    let refButton = document.createElement("a");
+    let buttonText = document.createElement("p");
+    buttonText.innerHTML = name.toUpperCase();
+    refButton.href = "./" + name + ".html";
+    refButton.id = name + "-button";
+    refButton.className = "header-hyperlinks";
+    refButton.appendChild(buttonText);
+    return refButton;
+}
+
+function loadNavbarButtons() {
+    let headerNav = document.getElementsByClassName("header-nav");
+    let headerLoginNav = document.getElementsByClassName("header-login-register");
     let access_token = localStorage.getItem("access_token");
-
-    // Hacer proceso inverso (agregar al DOM)
-    if (access_token != null) {
-        loginButton.remove();
-        registerButton.remove();
+    
+    
+    if (headerNav.length >= 1 && headerLoginNav.length >= 1) {
+        headerNav[0].appendChild(createNavButton("index"));
+        console.log(headerNav[0]);
+        if (access_token != null) {
+            headerNav[0].appendChild(createNavButton("games"));
+            headerLoginNav[0].appendChild(createNavButton("logout"));
+        }
+        else {
+            headerLoginNav[0].appendChild(createNavButton("login"));
+            headerLoginNav[0].appendChild(createNavButton("register"));
+        }
     }
-    else {
-        gamesButton.remove();
-        logoutButton.remove();
-    }
+}
 
-    logoutButton.addEventListener("click", function (e) {
-        e.preventDefault();
-        console.log("logged out");
-        localStorage.removeItem("refresh_token");
-        localStorage.removeItem("access_token");
-        window.location = "./login.html";
-    });
+window.addEventListener("load", function (e) {
+    console.log("loaded DOM");
+    loadErrorBlock();
+    loadNavbarButtons();
+    let logoutButton = document.getElementById("logout-button");
+
+    if (logoutButton != null) {
+        logoutButton.addEventListener("click", function (e) {
+            e.preventDefault();
+            console.log("logged out");
+            localStorage.removeItem("refresh_token");
+            localStorage.removeItem("access_token");
+            window.location = "./login.html";
+        });
+    }
 }, false);
